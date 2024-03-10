b0a2e98dc56a61c861a42d050b55822965a23cff
Victor Martins <vhprestes@gmail.com>
2024-03-10 15:10:00 -0300

    feat: add filter by payment date

--- assets/js/app.js
+++ assets/js/app.js
@@ -76,3 +76,28 @@ function filterCobranca(month) {
     }
   }
 }
+
+const paymentMonth = document.getElementById('month-payment');
+paymentMonth.addEventListener('change', function() {
+  const value = this.value;
+  console.log('Mês de pagamento: ', value);
+  filterPayment(value);
+});
+
+function filterPayment(month) {
+  const table = document.getElementsByClassName('table-notas')[0];
+  const rows = table.getElementsByTagName('tr');
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    const columns = row.getElementsByTagName('td');
+    const column = columns[4];
+    if (column) {
+      const text = column.textContent || column.innerText;
+      const monthFromText = text.split('/')[1];
+      if (monthFromText == month) {
+      } else {
+        row.style.display = 'none';
+      }
+    }
+  }
+}
